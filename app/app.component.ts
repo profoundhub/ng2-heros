c0a05d452ebfa3bd2e2d6d5893e12330c11b08e1
Daniel Lim <hbt@markhamboards.com>
2016-07-22 20:15:23 -0400

    pt1-b

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+
+}
+
+
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
-}
-
-export class Hero {
-  id: number;
-  name: string;
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
