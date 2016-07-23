d28396d07de2d2e09d82e4a8fe8b825eed214e9d
Daniel Lim <hbt@markhamboards.com>
2016-07-22 23:26:03 -0400

    pt5 x1

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -5,8 +5,8 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-dashboard',
-  templateUrl: 'app/dashboard.component.html'
-  styleUrls:  ['app/dashboard.component.css']
+  templateUrl: 'app/dashboard.component.html',
+  styleUrls: ['app/dashboard.component.css']
 })
 
 export class DashboardComponent implements OnInit {
