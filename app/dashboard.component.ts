29f7b6b34ea24235f3dda4aca64bb68cc3e6046b
Daniel Lim <hbt@markhamboards.com>
2016-07-22 23:25:08 -0400

    pt5 x

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'my-dashboard',
   templateUrl: 'app/dashboard.component.html'
+  styleUrls:  ['app/dashboard.component.css']
 })
 
 export class DashboardComponent implements OnInit {
@@ -24,5 +25,5 @@ export class DashboardComponent implements OnInit {
     let link = ['/detail', hero.id];
     this.router.navigate(link);
   }
-  
+
 }
