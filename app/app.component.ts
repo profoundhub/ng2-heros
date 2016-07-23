7a0a93142718b396edcb9d36337614c866761d13
Daniel Lim <hbt@markhamboards.com>
2016-07-22 22:22:37 -0400

    pt5 h

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { HeroService } from './hero.service';
-import { HeroesComponent } from './heroes.component';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { HeroesComponent } from './heroes.component';
 
 @Component({
   selector: 'my-app',
   template: `
    <h1>{{title}}</h1>
    <a [routerLink]="['/heroes']">Heroes</a>
-   <router-outlet></router-outlet>   
+   <router-outlet></router-outlet>
    `,
    directives: [HeroesComponent, ROUTER_DIRECTIVES],
    providers: [HeroService]
