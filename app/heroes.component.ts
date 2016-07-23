c8d91fc9ce2729ea554f930cc5e53728d4088612
Daniel Lim <hbt@markhamboards.com>
2016-07-22 23:11:45 -0400

    pt5 ww6

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -6,19 +6,25 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-heroes',
   templateUrl: 'app/heroes.component.html',
-  stylesUrls: 'app/heroes.component.css',
+  styleUrls: ['app/heroes.component.css'],
   directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
+
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
+
   constructor(private heroService: HeroService) { }
+
   getHeroes() {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+
   ngOnInit() {
     this.getHeroes();
   }
+
   onSelect(hero: Hero) { this.selectedHero = hero; }
+  
 }
