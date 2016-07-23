804c58a91db370ad39069ce70a6f977daf498864
Daniel Lim <hbt@markhamboards.com>
2016-07-22 22:31:19 -0400

    pt5 m

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-dashboard',
