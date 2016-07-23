85419629ce98fa923a3de8e5bdb19f7dbb8c4cdc
Daniel Lim <hbt@markhamboards.com>
2016-07-22 22:33:50 -0400

    pt5 o

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,16 @@
 import { provideRouter, RouterConfig }  from '@angular/router';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component'
+import { HeroDetailComponent } from './hero-detail.component';
+
+
 const routes: RouterConfig = [
   {
     path: 'heroes',
     component: HeroesComponent
+  }, {
+  path: 'detail/:id',
+  component: HeroDetailComponent
   }, {
   path: '',
   redirectTo: '/dashboard',
