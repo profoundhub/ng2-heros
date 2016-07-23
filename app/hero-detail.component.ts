2083f8ffc9a824e93e2c1a335819adf881767142
Daniel Lim <hbt@markhamboards.com>
2016-07-22 22:46:58 -0400

    pt5 r

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -5,16 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'my-hero-detail',
-  template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} Details:</h2>
-    <div><label>ID: </label>{{hero.id}}</div>
-    <div>
-      <label>Name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>
-`
+  templateUrl: 'app/hero-detail.component.html',
 })
 
 export class HeroDetailComponent implements OnInit, OnDestroy {
