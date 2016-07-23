53796010c916a04d9186477a1add8994b187ad5a
Daniel Lim <hbt@markhamboards.com>
2016-07-22 22:26:20 -0400

    pt5 i

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,8 @@ import { HeroesComponent } from './heroes.component';
   selector: 'my-app',
   template: `
    <h1>{{title}}</h1>
-   <a [routerLink]="['/heroes']">Heroes</a>
+   <a [routerLink]="['/heroes']">Heroes</a> | 
+   <a [routerLink]="['/dashboard']">Dashboard</a>
    <router-outlet></router-outlet>
    `,
    directives: [HeroesComponent, ROUTER_DIRECTIVES],
--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,17 @@
 import { provideRouter, RouterConfig }  from '@angular/router';
 import { HeroesComponent } from './heroes.component';
-
+import { DashboardComponent } from './dashboard.component'
 const routes: RouterConfig = [
   {
     path: 'heroes',
     component: HeroesComponent
+  }, {
+  path: '',
+  redirectTo: '/dashboard',
+  pathMatch: 'full'
+  }, {
+    path: 'dashboard',
+    component: DashboardComponent
   }
 ];
 
