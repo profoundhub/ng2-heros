95e04ff651d36ca8b63d23ac266e2b06cdc58518
Daniel Lim <hbt@markhamboards.com>
2016-07-22 22:31:55 -0400

    pt5 n

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -6,4 +6,13 @@ import { HeroService } from './hero.service';
   selector: 'my-dashboard',
   templateUrl: 'app/dashboard.component.html'
 })
-export class DashboardComponent { }
+
+export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) { }
+  ngOnInit() {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+  gotoDetail() { /* not implemented yet */}
+}
