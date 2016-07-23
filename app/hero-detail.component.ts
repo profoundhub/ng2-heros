e28df1fcdeb695eb61f329255d6b1ec45d6dd6e9
Daniel Lim <hbt@markhamboards.com>
2016-07-22 20:40:23 -0400

    pt3 b

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,26 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'my-hero-detail',
-
   template: `
   <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
+    <h2>{{hero.name}} Details:</h2>
+    <div><label>ID: </label>{{hero.id}}</div>
     <div>
-      <label>name: </label>
+      <label>Name: </label>
       <input [(ngModel)]="hero.name" placeholder="name"/>
     </div>
   </div>
 `
 })
+
 export class HeroDetailComponent {
-
+  @Input()
   hero: Hero;
-
-}
-
-export class Hero {
-  id: number;
-  name: string;
 }
