4f4dc6a0afe296ffc5856c264218ce5450650901
Daniel Lim <hbt@markhamboards.com>
2016-07-22 22:11:34 -0400

    pt5 c

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+
+@Component({
+  selector: 'my-app',
+  template: `
+   <h1>{{title}}</h1>
+   <my-heroes></my-heros>
+   `,
+   directives: [HeroesComponent],
+   providers: [HeroService]
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
