ba276f47c1e718f513c31375f7e45d79e8e4650d
Daniel Lim <hbt@markhamboards.com>
2016-07-24 14:16:50 -0400

    part7 - http - e

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 
