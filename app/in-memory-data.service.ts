635c894a6fb19ab2f293d513650092ed67b3cd78
Daniel Lim <hbt@markhamboards.com>
2016-07-24 16:26:52 -0400

    fresh updates part 5 - u

--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -2,15 +2,15 @@ export class InMemoryDataService {
   createDb() {
     let heroes = [
       {id: 11, name: 'Mr. Nice'},
-      {id: 12, name: 'Narco'},
-      {id: 13, name: 'Bombasto'},
-      {id: 14, name: 'Celeritas'},
-      {id: 15, name: 'Magneta'},
-      {id: 16, name: 'RubberMan'},
-      {id: 17, name: 'Dynama'},
-      {id: 18, name: 'Dr IQ'},
-      {id: 19, name: 'Magma'},
-      {id: 20, name: 'Tornado'}
+      {id: 12, name: 'Tarco'},
+      {id: 13, name: 'Bombastico'},
+      {id: 14, name: 'Mr. Boom'},
+      {id: 15, name: 'Ms Agenta'},
+      {id: 16, name: 'The Rubber Heart'},
+      {id: 17, name: 'Dynamama'},
+      {id: 18, name: 'Dr. IQ'},
+      {id: 19, name: 'The Bronze Brain'},
+      {id: 20, name: 'Magma Tornado'}
     ];
     return {heroes};
   }
