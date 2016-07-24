d63ac48072bc6759789b20af8ff4a70b988cee91
Daniel Lim <hbt@markhamboards.com>
2016-07-24 17:13:30 -0400

    clean up 3

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Hero } from './hero';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { Router } from '@angular/router';
+
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
@@ -17,7 +19,9 @@ export class HeroesComponent implements OnInit {
   title = 'Our Heros';
   heroes: Hero[];
   selectedHero: Hero;
-
+  addingHero = false;
+  error: any;
+  
   constructor(
     private router: Router,
     private heroService: HeroService) { }
