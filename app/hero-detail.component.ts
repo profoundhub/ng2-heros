01d1c2c09e6720c0352df01aad9823ffd80b8bd2
Daniel Lim <hbt@markhamboards.com>
2016-07-24 15:38:33 -0400

    fresh updates part 5 - G

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -10,9 +10,13 @@ import { HeroService } from './hero.service';
   templateUrl: 'app/hero-detail.component.html',
   styleUrls: ['app/hero-detail.component.css']
 })
+
 export class HeroDetailComponent implements OnInit, OnDestroy {
-  hero: Hero;
+  @Input() hero: Hero;
+  @Output() close = new EventEmitter();
+  error: any;
   sub: any;
+  navigated = false; // true if navigated here
 
   constructor(
     private heroService: HeroService,
@@ -21,9 +25,15 @@ export class HeroDetailComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-      let id = +params['id'];
-      this.heroService.getHero(id)
-        .then(hero => this.hero = hero);
+      if (params['id'] !== undefined) {
+        let id = +params['id'];
+        this.navigated = true;
+        this.heroService.getHero(id)
+            .then(hero => this.hero = hero);
+      } else {
+        this.navigated = false;
+        this.hero = new Hero();
+      }
     });
   }
 
@@ -33,5 +43,15 @@ export class HeroDetailComponent implements OnInit, OnDestroy {
 
   goBack() {
     window.history.back();
+  }
+
+  save() {
+    this.heroService
+        .save(this.hero)
+        .then(hero => {
+          this.hero = hero; // saved hero, w/ id if new
+          this.goBack(hero);
+        })
+        .catch(error => this.error = error); // TODO: Display error message
   }
 }
